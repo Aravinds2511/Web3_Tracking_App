e174108adbc3b381eadafcc3301be58577e92031
arvndblocker"nn <aravindvasang@gmail.com>
2023-11-06 14:00:49 +0530

    testnet_deployment

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0xc8bc14598D279CD091f146C1B8b8ed3D88df2Ea7"
+  export const contractAddress = "0x6fd780825D6cA6DBc3EB394444faa447B476f98b"
   export const ownerAddress = "0x48859b652d18afFA7Dbff03414DBD2FD4050d5Ec"
   
